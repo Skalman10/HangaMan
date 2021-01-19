211b72e8a2509d12b0e45c7b287f314ea5fcd3dd
Skalmanleif <isak.thyrel@elev.ga.ntig.se>
2021-01-19 10:15:29 +0100

    Har gjort en scanner för ordfilen

--- src/HängaGubbe.java
+++ src/HängaGubbe.java
@@ -1,8 +1,14 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class HängaGubbe {
     public static void main(String[] args) {
-
+        Scanner ord = null;
+        try {
+            ord = new Scanner(new File("src","OrdLista"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
